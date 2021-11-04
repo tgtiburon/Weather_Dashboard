df1cc5fb4c2245f1487246139802b4dfa36d9129
tgtiburon <tg.tiburon@gmail.com>
2021-11-03 20:28:59 -0400

    rough UI done

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+
+
+// let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
+//         axios.get(queryURL)
+//         .then(function(response){
+
+
+//#region Variables
+const APIKey = "14a2bf498ec7475c9f365cf46a525533";
